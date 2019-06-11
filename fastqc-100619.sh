840b66f2eb82d2fc16bdfc58a9cd6e215b6bdee8
edvinvoneuler <edvinvoneuler@gmail.com>
2019-06-11 11:49:46 +0200

    fastqc-100619.sh batchID 9081185

--- fastqc-100619.sh
+++ fastqc-100619.sh
@@ -1,12 +1,14 @@
+#!/bin/bash -l
 #SBATCH -A snic2018-3-449
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 1:00:00
-#SBATCH -J fastQC
+#SBATCH -J fastQC_Jena
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user edvinvoneuler@gmail.com
 
-load bioinfo-tools fastQC
+#module load bioinfo-tools
+#module load fastQC
 
 DATDIR=/home/edvo1850/DENTAL_CALC/gorilla_calculus
 OUTDIR=/home/edv01850/DENTAL_CALC/DENTAL_CALCULUS_JENA_GORILLA_181009/FASTQC
@@ -14,9 +16,9 @@ OUTDIR=/home/edv01850/DENTAL_CALC/DENTAL_CALCULUS_JENA_GORILLA_181009/FASTQC
 
 for file in ${DATDIR}/*/*
 do	
-	name=${file##*/}
-        base=${name%.fastq.gz}
-	mkdir ${OUTDIR}/$base 	
-    	fastqc $file -o ${OUTDIR}/$base
-	#echo  $file -o ${OUTDIR}/$base
+	sample=$(basename $(dirname $file))
+	read=$(basename $file)
+	mkdir -p $OUTDIR/$sample/$read
+    	fastqc $file -o $OUTDIR/$sample/$read
+	#echo $OUTDIR/$sample/$read
 done
