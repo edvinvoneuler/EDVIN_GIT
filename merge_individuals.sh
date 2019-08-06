7e786364edb767d40df48489c1fb35ce041d7fe2
edvinvoneuler <edvinvoneuler@gmail.com>
2019-08-06 15:32:35 +0200

    conc files that belong to the same individuals

--- merge_individuals.sh
+++ merge_individuals.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#!/bin/bash -l
+#SBATCH -A snic2018-3-449
+#SBATCH -p core
+#SBATCH -n 4
+#SBATCH -t 4:00:00
+#SBATCH -J merge_individuals
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user edvinvoneuler@gmail.com
 
 outdir=/home/edvo1850/DENTAL_CALC/DENTAL_CALCULUS_JENA_GORILLA_181009/P6_merged_samples_unmapped
-for file in /home/edvo1850/DENTAL_CALC/DENTAL_CALCULUS_JENA_GORILLA_181009/P6_data_unmapped_human
+for file in /home/edvo1850/DENTAL_CALC/DENTAL_CALCULUS_JENA_GORILLA_181009/P6_data_unmapped_human/*.fastq
 do
     basename=$(basename $file)
     ind_name=$(echo $basename | cut -c1-6)
-
-    cat $file >> $outdir/$ind_name
+    
+    echo "Input file:" $file "Concatenated into" $outdir/$ind_name.fastq
+    cat $file >> $outdir/$ind_name.fastq
+done
