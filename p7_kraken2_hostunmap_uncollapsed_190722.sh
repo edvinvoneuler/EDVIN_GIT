962290043d13b133a334c2084182dec8cb8c696f
edvinvoneuler <edvinvoneuler@gmail.com>
2019-08-06 15:25:46 +0200

    preprocessing - uncollapsed reads

---
+++ p7_kraken2_hostunmap_uncollapsed_190722.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+
+#SBATCH -A snic2018-3-449
+#SBATCH -p node
+#SBATCH -n 1
+#SBATCH -C mem256GB
+#SBATCH -t 48:00:00
+#SBATCH -J kraken_jena_unmapped-host_merged
+
+# Microbial assignments using kraken
+# Standard kraken database (RefSeq bacteria+archaea+viral+human)
+# build by UPPMAX $KRAKEN_DB (updated started of each month)
+
+# Unmerged and merged
+
+module load bioinfo-tools Kraken2
+echo $(module list)
+echo $(readlink -f $KRAKEN2_DEFAULT_DB)
+
+DATDIR=/crex/proj/sllstore2017021/nobackup/JAELLE/DENTAL_CALCULUS_JENA_GORILLA_181009/P6_merged_samples_unmapped_uncollapsed
+OUTDIR=/crex/proj/sllstore2017021/nobackup/JAELLE/DENTAL_CALCULUS_JENA_GORILLA_181009/P7_kraken2_uncollapsed_190722
+
+# Set up database
+MY_DB_DIR=$SNIC_TMP/Kraken2
+MY_DB=$MY_DB_DIR/${KRAKEN2_DEFAULT_DB##*/}
+mkdir -p $MY_DB
+cp -av $KRAKEN2_DEFAULT_DB/* $MY_DB/
+
+
+mkdir $OUTDIR
+cd $DATDIR
+
+for i in *.fastq
+do
+	sample=${i%.fastq}
+	echo "Sample: $sample"
+    kraken2 --db $MY_DB $i --threads 20 --fastq-input --report $OUTDIR/${sample}_kraken2_report.txt --report-zero-counts --output $OUTDIR/${sample}_kraken2_output.txt;
+done
